5b6da919050c794fd4232c4c780198f66d7cab74
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-02-20 08:39:24 +0300

    test_bot

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+from json.decoder import JSONDecodeError
 
 import requests
 import telegram
@@ -29,14 +30,20 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-logging.info('Бот запущен!')
 
+class HomeworkbotException(Exception):
+    pass
 
 def send_message(bot, message):
     """Функция отправки сообщения."""
     chat_id = TELEGRAM_CHAT_ID
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    bot.send_message(chat_id, text=message)
+    try:
+        bot.send_message(chat_id, text=message)
+        logging.info('Сообщение отправлено.')
+    except Exception as error:
+        message = f'Ошибка отправки сообщения: {error}'
+        logging.error(message)
+        raise HomeworkbotException(message)
 
 
 def get_api_answer(current_timestamp):
@@ -48,14 +55,17 @@ def get_api_answer(current_timestamp):
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != 200:
             message = 'API недоступен: status code is not 200'
-            logging.error(message)
-            raise Exception(message)
-        logging.info("Получен ответ от сервера")
-        return response.json()
-    except Exception as error:
+            raise HomeworkbotException(message)
+    except ConnectionError as error:
         message = f'Ошибка обращения к API: {error}'
         logging.error(message)
-        raise Exception(message)
+        raise HomeworkbotException(message)
+    try:
+        return response.json()
+    except JSONDecodeError:
+        raise HomeworkbotException(
+            'Ответ должен быть в формате JSON'
+        )
 
 
 def check_response(response):
@@ -79,19 +89,15 @@ def parse_status(homework):
     homework_name = homework['homework_name']
     homework_status = homework['status']
     verdict = HOMEWORK_STATUSES[homework_status]
-    if homework_status not in HOMEWORK_STATUSES or None:
-        logging.error('Отсутствует статус')
-        raise Exception('Отсутствует статус')
-    else:
-        logging.info('Список домашних работ получен')
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    logging.info('Список домашних работ получен')
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверка наличия токенов."""
-    if PRACTICUM_TOKEN is None or \
-            TELEGRAM_TOKEN is None or \
-            TELEGRAM_CHAT_ID is None:
+    if (PRACTICUM_TOKEN  is None or
+            TELEGRAM_TOKEN is None or
+            TELEGRAM_CHAT_ID is None):
         return False
     return True
 
@@ -100,22 +106,27 @@ def main():
     """Основная логика работы бота."""
     if not check_tokens():
         logging.critical('Отсутствует обязательная переменная окружения')
-        return False
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+    logging.info('Бот запущен!')
+    response = get_api_answer(current_timestamp)
     while True:
         try:
-            response = get_api_answer(current_timestamp)
             homework = check_response(response)
-            message = parse_status(homework)
-            time.sleep(RETRY_TIME)
+            message = parse_status(homework[0])
+            logging.info('Отсутствуют новые статусы')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(message)
-            time.sleep(RETRY_TIME)
+            old_message = ''
+            if message != old_message:
+                send_message(bot, message) 
+                logging.info('Сообщение с ошибкой отправлено.')
+            old_message = message
         else:
             send_message(bot, message)
-
-
+        finally:
+            time.sleep(RETRY_TIME)
+    
 if __name__ == '__main__':
     main()
